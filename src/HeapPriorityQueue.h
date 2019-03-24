d941a495e59e9679128d5d9b58bd7ab11b73dabd
Apurba <apurba10024@gmail.com>
2019-03-25 05:22:02 +0600

    2.9 Updated Comment

--- src/HeapPriorityQueue.h
+++ src/HeapPriorityQueue.h
@@ -1,8 +1,3 @@
-// This is a .h file you will edit and turn in.
-// We have provided a skeleton for you,
-// but you must finish it as described in the spec.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
 
 #ifndef _heappriorityqueue_h
 #define _heappriorityqueue_h
@@ -12,21 +7,18 @@
 #include "PQEntry.h"
 using namespace std;
 
-/*
- * For documentation of each member, see VectorPriorityQueue.h.
- */
 class HeapPriorityQueue {
 public:
     HeapPriorityQueue();
     ~HeapPriorityQueue();
-    void changePriority(string value, int newPriority);
-    void clear();
-    string dequeue();
-    void enqueue(string value, int priority);
-    bool isEmpty() const;
-    string peek() const;
-    int peekPriority() const;
-    int size() const;
+    void changePriority(string value, int newPriority); // Complexity - O(n + log(n))
+    void clear(); // Complexity - O(n)
+    string dequeue(); // Complexity - O(log(n))
+    void enqueue(string value, int priority); // Complexity - O(log(n))
+    bool isEmpty() const; // Complexity - O(1)
+    string peek() const; // Complexity - O(1)
+    int peekPriority() const; // Complexity - O(1)
+    int size() const; // // Complexity - O(1)
     friend ostream& operator <<(ostream& out, const HeapPriorityQueue& queue);
 
 private:
